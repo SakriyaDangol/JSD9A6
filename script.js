e84ea26b592e1b9e53eca1f9924dc81914cf8ee7
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-15 16:50:58 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+myObj = {
+    apple:"red",
+    ball:"round",
+    cat:"meow",
+    dog:"bone"
+}
+
+    console.log(myObj.apple + '\n' + myObj.ball + '\n' + myObj.cat + '\n' + myObj.dog );
+
+   // another way
+    // var txt = "";
+    // for (let y in myObj) {
+    // txt += myObj[y] + "\n";
+    // }
+    // console.log(txt);
